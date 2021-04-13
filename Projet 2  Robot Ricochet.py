f14a77e4a4b3e877e766cebbdae35424a47619df
uvsq22010110 <71687783+uvsq22010110@users.noreply.github.com>
2021-04-13 09:32:39 +0200

    Update Projet 2  Robot Ricochet.py

--- Projet 2  Robot Ricochet.py
+++ Projet 2  Robot Ricochet.py
@@ -1,1 +1,13 @@
+##################################
+# Groupe 1  de BI TD3
+
+# Auteurs: 
+# Coraline PELON
+# Jacques-Henri Lartigue 
+# Nojimba Ahamada
+# Alice Dumontroty
+# Mathieu Chekroun
+# Desira Junior Ebelebe
+
+# lien du github:https://github.com/uvsq22010110/Projet-2-Robot-Ricochet.git
 print("hello")
