c364638ca7fcb9a6e138372ad7a24cffb080dbfc
Jacques-henri LARTIGUE <jacques-henri.lartigue@ens.uvsq.fr>
2021-04-18 13:48:04 +0200

    Update Projet 2  Robot Ricochet.py

--- Projet 2  Robot Ricochet.py
+++ Projet 2  Robot Ricochet.py
@@ -10,4 +10,14 @@
 # Desira Junior Ebelebe
 
 # lien du github:https://github.com/uvsq22010110/Projet-2-Robot-Ricochet.git
+
+# import des modules
+
+import tkinter as tk
+
+# constantes
+
+HAUTEUR = 390
+LARGEUR = 600
+COULEUR_FOND = "grey60"
 print("hello")
