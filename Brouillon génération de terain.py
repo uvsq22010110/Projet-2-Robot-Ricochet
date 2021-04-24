9de85ab0c16eeeb8ab3c5e5effffb6c6dfb3f14a
N.AHAMADA <71550445+uvsq22000221@users.noreply.github.com>
2021-04-24 12:57:46 +0200

    Update Brouillon génération de terain.py
    Création bordure épaisse

--- Brouillon génération de terain.py
+++ Brouillon génération de terain.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+
 import random as rd
 
 HAUTEUR = 640
@@ -15,8 +16,6 @@ COULEUR_ROBOT3 = "blue"
 COULEUR_ROBOT4 = "yellow"
 COULEUR_CIBLE = "aleatoire"
 
-
-
 def quadrillage():
     global COULEUR_QUADRILLAGE
     x0, x1 = 0, LARGEUR
@@ -30,55 +29,28 @@ def quadrillage():
         canvas.create_line(x, y0, x, y1, fill=COULEUR_QUADRILLAGE)
         x += COTE
 
+
 def coord_to_lg(x, y):
     
-    return x // COTE, y // COTE        
+    return x // COTE, y // COTE
 
 
-def creer_tableau():
+def bordure():
+    mur1 = canvas.create_line(0,0,0,639, fill="black", width = '9')
+    mur2 = canvas.create_line(0,0,639,0, fill="black", width = '9')
+    mur3 = canvas.create_line(639,0,639,639, fill="black", width = '9')
+    mur4 = canvas.create_line(0,639,639,639, fill="black", width = '9' )
+
     
-    global tableau
-    tableau = []
-    for i in range(NB_COL):
-        tableau.append([-1] * NB_LINE)
-        
-def creer_robot(couleur_robot):
-    "dessine robot"
-    i = rd.randint(0,15)
-    j = rd.randint(0,15)
-    #attention pas sous le carré noir
-    x,y = i*COTE, j*COTE
-    dx,dy = 10, 10
-    rayon = COTE
-    cercle = canvas.create_oval((x, y), (x+rayon, y+rayon), fill = couleur_robot)
-    return [cercle, dx, dy]
+    
 
 
-def creer_carrer():
-    "dessine carrer"
-    i = 16 // 2
-    j = 16 // 2 + 1
-    carrer = canvas.create_rectangle((40*(i - 1), 40*(i - 1)), (40*j, 40*j), fill = "black")
+root = Tk()
+canvas = Canvas(root, width=640, height=640, borderwidth=0, highlightthickness=0, bg = COULEUR_FOND)
+canvas.grid(column = 1, row = 0)
+canvas.pack()
 
 
-
-
-
-# programme principal
-racine = tk.Tk()
-racine.title("Jeu des robots")
-
-
-# création des widgets
-canvas = tk.Canvas(racine, width= LARGEUR, height= HAUTEUR, bg= "navajo white")
-
-canvas.grid()
-
-creer_robot(COULEUR_ROBOT1)
-creer_robot(COULEUR_ROBOT2)
-creer_robot(COULEUR_ROBOT3)
-creer_robot(COULEUR_ROBOT4)
-
-creer_carrer()
 quadrillage()
-racine.mainloop()
+bordure()    
+root.mainloop()
