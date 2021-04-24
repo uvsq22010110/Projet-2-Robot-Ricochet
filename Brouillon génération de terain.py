41d85119c3c7a4a291ce9f1efb64a2dc85f47cd2
N.AHAMADA <71550445+uvsq22000221@users.noreply.github.com>
2021-04-24 13:04:58 +0200

    Mise en place de bordures externes

--- Brouillon génération de terain.py
+++ Brouillon génération de terain.py
@@ -36,21 +36,58 @@ def coord_to_lg(x, y):
 
 
 def bordure():
+    """creation bordures"""
     mur1 = canvas.create_line(0,0,0,639, fill="black", width = '9')
-    mur2 = canvas.create_line(0,0,639,0, fill="black", width = '9')
+    mur2 = canvas.create_line(0,0,639,0, fill="black", width = '9') 
     mur3 = canvas.create_line(639,0,639,639, fill="black", width = '9')
     mur4 = canvas.create_line(0,639,639,639, fill="black", width = '9' )
 
+
+def creer_tableau():
     
-    
+    global tableau
+    tableau = []
+    for i in range(NB_COL):
+        tableau.append([-1] * NB_LINE)
+        
+def creer_robot(couleur_robot):
+    "dessine robot"
+    i = rd.randint(0,15)
+    j = rd.randint(0,15)
+    #attention pas sous le carré noir
+    x,y = i*COTE, j*COTE
+    dx,dy = 10, 10
+    rayon = COTE
+    cercle = canvas.create_oval((x, y), (x+rayon, y+rayon), fill = couleur_robot)
+    return [cercle, dx, dy]
 
 
-root = Tk()
-canvas = Canvas(root, width=640, height=640, borderwidth=0, highlightthickness=0, bg = COULEUR_FOND)
+def creer_carrer():
+    "dessine carrer"
+    i = 16 // 2
+    j = 16 // 2 + 1
+    carrer = canvas.create_rectangle((40*(i - 1), 40*(i - 1)), (40*j, 40*j), fill = "black")
+
+
+
+##########Programme Principale##########################################################################
+racine = tk.Tk()
+racine.title("Jeu des robots")
+
+
+canvas = Canvas(racine, width=640, height=640, borderwidth=0, highlightthickness=0, bg = COULEUR_FOND)
 canvas.grid(column = 1, row = 0)
 canvas.pack()
 
 
+creer_robot(COULEUR_ROBOT1)
+creer_robot(COULEUR_ROBOT2)
+creer_robot(COULEUR_ROBOT3)
+creer_robot(COULEUR_ROBOT4)
+
+
+
+creer_carrer()
 quadrillage()
 bordure()    
 root.mainloop()
